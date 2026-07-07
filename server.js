551d7a589a8d4577885dda8155eadcbd196bd958
GABRIEL HENRIQUE SALM DE BORBA <gabriel_hs_borba@estudante.sesisenai.org.br>
2026-07-07 15:25:24 -0300

    arrumando

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const database = require('./src/config/database');
+const express = require('express');
+const Routes = require('./src/config/routes');
+
+database.authenticate();
+
+let app = express();
+app.use(express.json());
+Routes(app);
+
+database.sync({ alter: true });
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
